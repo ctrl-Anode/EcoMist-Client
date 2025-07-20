dab5123ddcb9c23d43db0f13ec3c710099042e59
ctrl-Anode <arnoldadeva001@gmail.com>
2025-07-20 16:13:53 +0800

    storage

--- dist/assets/FacebookIcon-8661lqQH.js
+++ dist/assets/FacebookIcon-8661lqQH.js
@@ -1,4 +1,4 @@
-import{_ as r,c as n,o,a as t}from"./main-BTy6GRN3.js";const s={},c={xmlns:"http://www.w3.org/2000/svg",class:"w-5 h-5 flex-shrink-0 text-green-600 mt-1",fill:"currentColor",viewBox:"0 0 24 24"};function a(l,e){return o(),n("svg",c,e[0]||(e[0]=[t("path",{d:`M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325\r
+import{_ as r,c as n,o,a as t}from"./main-BpIfdtAQ.js";const s={},c={xmlns:"http://www.w3.org/2000/svg",class:"w-5 h-5 flex-shrink-0 text-green-600 mt-1",fill:"currentColor",viewBox:"0 0 24 24"};function a(l,e){return o(),n("svg",c,e[0]||(e[0]=[t("path",{d:`M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325\r
       24h11.495v-9.294H9.691v-3.622h3.129V8.413c0-3.1\r
       1.893-4.788 4.659-4.788 1.325 0 2.464.099\r
       2.795.143v3.24h-1.918c-1.504 0-1.796.715-1.796\r
--- src/components/EcoLoader/EcoSpinner.vue
+++ src/components/EcoLoader/EcoSpinner.vue
@@ -1,12 +1,22 @@
 <template>
-  <div
-    class="spinner"
-    :style="spinnerStyle"
-    role="status"
-    aria-busy="true"
+  <div 
+    class="loader-container"
     :class="{ centered }"
+    role="status" 
+    aria-busy="true"
+    aria-label="Loading"
   >
-    <div v-for="n in 10" :key="n" :style="ringStyle(n)"></div>
+    <div 
+      class="loader"
+      :style="loaderStyle"
+    >
+      <div 
+        v-for="n in 8" 
+        :key="n" 
+        class="dot"
+        :style="dotStyle(n)"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -15,58 +25,102 @@ import { computed } from 'vue'
 
 const props = defineProps({
   size: {
-    type: String,
-    default: '40px'
+    type: [String, Number],
+    default: 32
   },
   color: {
     type: String,
-    default: '#000'
+    default: '#3b82f6'
   },
   centered: {
     type: Boolean,
     default: true
+  },
+  speed: {
+    type: Number,
+    default: 1.2
   }
 })
 
-const spinnerStyle = computed(() => ({
-  width: props.size,
-  height: props.size
+const normalizedSize = computed(() => 
+  typeof props.size === 'number' ? `${props.size}px` : props.size
+)
+
+const loaderStyle = computed(() => ({
+  width: normalizedSize.value,
+  height: normalizedSize.value,
+  '--speed': `${props.speed}s`
 }))
 
-const ringStyle = (n) => ({
-  '--rotation': `${n * 36}`,
-  '--delay': `${n * 0.1}`,
-  '--translation': '150',
-  background: props.color
+const dotStyle = (n) => ({
+  '--delay': `${(n - 1) * 0.15}s`,
+  backgroundColor: props.color
 })
 </script>
 
 <style scoped>
-.spinner {
-  position: relative;
+.loader-container {
+  display: inline-block;
 }
+
 .centered {
-  position: absolute;
+  position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  z-index: 9999;
 }
-.spinner div {
+
+.loader {
+  position: relative;
+  display: inline-block;
+}
+
+.dot {
   position: absolute;
-  width: 50%;
-  height: 150%;
-  transform: rotate(calc(var(--rotation) * 1deg)) translate(0, calc(var(--translation) * 1%));
-  animation: spinner-rotate 1s calc(var(--delay) * 1s) infinite ease;
-  border-radius: 2px;
+  width: 12.5%;
+  height: 12.5%;
+  border-radius: 50%;
+  animation: pulse var(--speed) var(--delay) infinite ease-in-out;
 }
 
-@keyframes spinner-rotate {
-  0%, 10%, 20%, 30%, 50%, 60%, 70%, 80%, 90%, 100% {
-    transform: rotate(calc(var(--rotation) * 1deg)) translate(0, calc(var(--translation) * 1%));
-  }
+.dot:nth-child(1) { top: 0; left: 43.75%; }
+.dot:nth-child(2) { top: 12.5%; right: 12.5%; }
+.dot:nth-child(3) { top: 43.75%; right: 0; }
+.dot:nth-child(4) { bottom: 12.5%; right: 12.5%; }
+.dot:nth-child(5) { bottom: 0; left: 43.75%; }
+.dot:nth-child(6) { bottom: 12.5%; left: 12.5%; }
+.dot:nth-child(7) { top: 43.75%; left: 0; }
+.dot:nth-child(8) { top: 12.5%; left: 12.5%; }
 
-  50% {
-    transform: rotate(calc(var(--rotation) * 1deg)) translate(0, calc(var(--translation) * 1.5%));
+@keyframes pulse {
+  0%, 80%, 100% {
+    transform: scale(0.3);
+    opacity: 0.3;
+  }
+  40% {
+    transform: scale(1);
+    opacity: 1;
   }
 }
+
+/* Alternative spinner style - uncomment to use */
+/*
+.loader {
+  border: 2px solid rgba(0, 0, 0, 0.1);
+  border-left-color: v-bind(color);
+  border-radius: 50%;
+  animation: spin var(--speed) linear infinite;
+}
+
+.dot {
+  display: none;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+*/
 </style>
